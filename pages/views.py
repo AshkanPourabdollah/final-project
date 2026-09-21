2cc3f99392d3e26fd057daf2300b0c846fa186f7
AshkanPourabdollah <ashkanp09@gmail.com>
2026-09-21 14:55:57 -0700

    Add pages html file and finalizing all page app

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render, HttpResponse
 
+from products.models import Product
+
 
 # Create your views here.
 def home_view(request):
-    return render(request, 'pages/home_page.html')
+    products = Product.objects.all()[2:10]
+    return render(request, 'pages/home_page.html', context={'products': products})
+
+def contact_view(request):
+    return render(request, 'pages/contact_page.html')
+
+def about_view(request):
+    return render(request, 'pages/about_page.html')
--- products/models.py
+++ products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    SIZE_CHOICES = [
+        ('1', 'Small'),
+        ('2', 'Medium'),
+        ('3', 'Large'),
+        ('4', 'Extra Large'),
+    ]
+
+    title = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
+    sold_count = models.PositiveIntegerField(default=0)
+    rating_count = models.PositiveIntegerField(default=0)
+    short_description = models.TextField()
+    size = models.CharField(max_length=1, choices=SIZE_CHOICES)
+    weight = models.PositiveIntegerField(default=0)
+    price = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
